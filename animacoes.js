6692e8514a568ec557bafd47b96f8a5893fc4d7d
Victor <v_r_pessoa@hotmail.com>
2024-07-18 20:42:19 -0300

    Forçando deploy
    Tentativa de forçar um deploy

--- animacoes.js
+++ animacoes.js
@@ -3,17 +3,15 @@ $(document).ready(function(){
     if ($('#overlay').length === 0) {
         $('body').append('<div id="overlay"></div>');
     }
-
     const overlay = $('#overlay');
     const imgzoom = $('<img id="imgzoom">');
-    $('body').append(imgzoom);
 
+    $('body').append(imgzoom);
     $('.imgbloco').click(function() {
         const src = $(this).attr('src');
         imgzoom.attr('src', src);
         overlay.css('display', 'block');
         imgzoom.css('display', 'block');
-
         setTimeout(function() {
             overlay.css('opacity', 1);
             imgzoom.css('transform', 'translate(-50%, -50%) scale(1)');
